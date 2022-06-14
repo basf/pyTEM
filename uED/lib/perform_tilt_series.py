26806258a60a6a2ea55e5e0ea705d02af7e4f53d
Michael Luciuk <michael.luciuk@basf.com>
2022-06-14 16:04:13 +0200

    First try at a program to actually perform a tilt acquisition series.

--- uED/lib/perform_tilt_series.py
+++ uED/lib/perform_tilt_series.py
@@ -2,31 +2,121 @@
  Author:  Michael Luciuk
  Date:    Summer 2022
 """
-import math
+
+import threading
+import time
+
+import numpy as np
+from tifffile import tifffile
 
 
-def perform_tilt_series(microscope, camera, integration_time, binning, alpha_arr, shifts, out_file):
+def perform_tilt_series(microscope, acquisition_properties, shifts):
     """
     Actually perform a tilt series and save the returns to file.
 
     :param microscope: TEMInterface:
         A TEMInterface interface to the microscope.
-    :param camera: str:
-        The name of the camera being
-    :param integration_time: float:
-        Total exposure time for a single image in seconds.
-    :param binning: Photo resolution, one of:
-        - '4k' for 4k images (4096 x 4096; sampling=1)
-        - '2k' for 2k images (2048 x 2048; sampling=2)
-        - '1k' for 1k images (1024 x 1024; sampling=3)
-        - '0.5k' for 05.k images (512 x 512; sampling=8)
-    :param alpha_arr:
-    :param shifts:
-    :param out_file:
-    :return:
+    :param acquisition_properties: AcquisitionProperties:
+        The acquisition properties.
+    :param shifts: np.array of float tuples:
+        An array of tuples of the form (x, y) where x and y are the required image shifts (in microns) required to
+         align the image at the corresponding tilt.
+
+    :return: None.
+        Tilt series results are automatically saved to file.
     """
-    # microscope.set_stage_position(alpha=alpha_arr[0])  # Move to the start angle
+    # Take a quick test image just to get the image size
+    reference_acq = microscope.acquisition(camera_name=acquisition_properties.camera_name,
+                                           sampling=acquisition_properties.sampling, exposure_time=0.5)
+    reference_image = reference_acq.get_image()
+    image_shape = np.shape(reference_image)
+    image_stack_arr = np.full(shape=(len(acquisition_properties.alphas), image_shape[0], image_shape[1]),
+                              fill_value=np.nan)
 
-    # reference = microscope.single_acquisition(camera_name=camera, binning=binning, exposure_time=None, readout_area=None)
+    microscope.set_stage_position(alpha=acquisition_properties.alpha_arr[0])  # Move to the start angle
 
-    raise NotImplementedError
+    # So that tilting can happen simultaneously, it will be performed in a separate thread
+    tilting_thread = TiltingThread(microscope=microscope, acquisition_properties=acquisition_properties)
+    tilting_thread.start()  # Start tilting
+
+    # Iterate through the angles, taking pictures
+    for i in range(len(acquisition_properties.alphas)):
+        print("Taking image number: " + str(i))
+        microscope.set_image_shift(x=shifts[i][0], y=shifts[i][1])
+        acq = microscope.acquisition(camera_name=acquisition_properties.camera_name,
+                                     sampling=acquisition_properties.sampling,
+                                     exposure_time=acquisition_properties.integration_time)
+        image_stack_arr[i] = acq.get_image()
+
+    tilting_thread.join()
+
+    print("Saving data to file...")
+    tifffile.imwrite(acquisition_properties.out_file, image_stack_arr, metadata=reference_acq.get_metadata())
+
+
+class TiltingThread (threading.Thread):
+
+    def __init__(self, microscope, acquisition_properties):
+        threading.Thread.__init__(self)
+        self.microscope = microscope
+        self.acquisition_properties = acquisition_properties
+
+    def run(self):
+        print("Starting tilting at " + str(time.ctime(time.time())))
+
+        self.microscope.set_stage_position_alpha(self, alpha=self.acquisition_properties.alpha_arr[-1],
+                                                 speed=self.acquisition_properties.tilt_speed, movement_type="go")
+
+        print("Stopping tilting at " + str(time.ctime(time.time())))
+
+
+class TakingImagesThread (threading.Thread):
+
+    def __init__(self, microscope, acquisition_properties):
+        threading.Thread.__init__(self)
+        self.microscope = microscope
+        self.acquisition_properties = acquisition_properties
+
+    def run(self):
+        print("Starting taking images at " + str(time.ctime(time.time())))
+
+        self.microscope.set_stage_position_alpha(self, alpha=self.acquisition_properties.alpha_arr[-1],
+                                                 speed=self.acquisition_properties.tilt_speed, movement_type="go")
+
+        print("Stopping taking images at " + str(time.ctime(time.time())))
+
+
+if __name__ == "__main__":
+    import pathlib
+    import sys
+
+    from uED.lib.AcquisitionProperties import AcquisitionProperties
+
+    package_directory = pathlib.Path().resolve().parent.resolve().parent.resolve()
+    sys.path.append(str(package_directory))
+
+    try:
+        from Interface.TEMInterface import TEMInterface
+
+        scope = TEMInterface()
+    except BaseException as e:
+        print("Unable to connect to microscope, proceeding with None object.")
+        scope = None
+
+    start_alpha = -10
+    stop_alpha = 10
+    step_alpha = 1
+
+    num_alpha = int((stop_alpha - start_alpha) / step_alpha + 1)
+    alpha_arr = np.linspace(start=start_alpha, stop=stop_alpha, num=num_alpha, endpoint=True)
+
+    aqc_props = AcquisitionProperties(camera_name='BM-Ceta', alpha_arr=alpha_arr, integration_time=3, sampling='1k',
+                                      out_file=None)
+
+    shifts_ = np.full(shape=len(aqc_props.alphas), dtype=(float, 2), fill_value=0.0)
+
+    print("Shifts:")
+    print(shifts_)
+
+    perform_tilt_series(microscope=scope, acquisition_properties=aqc_props, shifts=shifts_)
+
