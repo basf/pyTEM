144ac283e14bb16c8c84b900a6a1c9c6f5792f3f
Michael Luciuk <michael.luciuk@basf.com>
2022-06-20 13:39:12 +0200

    Updated docstring

--- uED/lib/exit_script.py
+++ uED/lib/exit_script.py
@@ -2,6 +2,7 @@
  Author:  Michael Luciuk
  Date:    Summer 2022
 """
+
 import sys
 import warnings
 
@@ -9,12 +10,13 @@ import warnings
 def exit_script(microscope, status):
     """
     Return the microscope to a safe state and exit the MicroED script.
-    :param microscope: 
+    :param microscope: TEMInterface (or None):
+        The microscope interface.
     :param status: int:
-    Exit status, one of:
-        -1: Failure
-        0: Success
-        1: Early exit (script not yet complete)
+        Exit status, one of:
+            1: Early exit (script not yet complete)
+            0: Success
+            -1: Failure
     """
     if microscope is not None:
         warnings.warn("Returning the microscope to a safe state...")
