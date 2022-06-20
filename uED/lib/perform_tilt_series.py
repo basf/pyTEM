3e5d0b790c6c3162e7674dc3c440b811a8a27d3f
Michael Luciuk <michael.luciuk@basf.com>
2022-06-20 14:34:47 +0200

    Adding verbose parameter

--- uED/lib/perform_tilt_series.py
+++ uED/lib/perform_tilt_series.py
@@ -10,7 +10,7 @@ import numpy as np
 from tifffile import tifffile
 
 
-def perform_tilt_series(microscope, acquisition_properties, shifts):
+def perform_tilt_series(microscope, acquisition_properties, shifts, verbose):
     """
     Actually perform a tilt series and save the returns to file.
 
@@ -21,11 +21,15 @@ def perform_tilt_series(microscope, acquisition_properties, shifts):
     :param shifts: np.array of float tuples:
         An array of tuples of the form (x, y) where x and y are the required image shifts (in microns) required to
          align the image at the corresponding tilt.
+     :param verbose: bool:
+        Print out extra information. Useful for debugging.
 
     :return: None.
         Tilt series results are automatically saved to file.
     """
     # Take a quick test image just to get the image size
+    if verbose:
+        print("Taking a reference image to get the image size...")
     reference_acq = microscope.acquisition(camera_name=acquisition_properties.camera_name,
                                            sampling=acquisition_properties.sampling, exposure_time=0.5)
     reference_image = reference_acq.get_image()
@@ -36,12 +40,14 @@ def perform_tilt_series(microscope, acquisition_properties, shifts):
     microscope.set_stage_position(alpha=acquisition_properties.alpha_arr[0])  # Move to the start angle
 
     # So that tilting can happen simultaneously, it will be performed in a separate thread
-    tilting_thread = TiltingThread(microscope=microscope, acquisition_properties=acquisition_properties)
+    tilting_thread = TiltingThread(microscope=microscope, acquisition_properties=acquisition_properties,
+                                   verbose=verbose)
     tilting_thread.start()  # Start tilting
 
     # Iterate through the angles, taking pictures
-    for i in range(len(acquisition_properties.alphas)):
-        print("Taking image number: " + str(i))
+    for i, alpha in enumerate(acquisition_properties.alphas):
+        if verbose:
+            print("Taking image number " + str(i) + " at \u03B1=" + str(alpha))
         microscope.set_image_shift(x=shifts[i][0], y=shifts[i][1])
         acq = microscope.acquisition(camera_name=acquisition_properties.camera_name,
                                      sampling=acquisition_properties.sampling,
@@ -50,40 +56,28 @@ def perform_tilt_series(microscope, acquisition_properties, shifts):
 
     tilting_thread.join()
 
-    print("Saving data to file...")
+    if verbose:
+        print("Saving image stack to file.")
     tifffile.imwrite(acquisition_properties.out_file, image_stack_arr, metadata=reference_acq.get_metadata())
 
 
 class TiltingThread (threading.Thread):
 
-    def __init__(self, microscope, acquisition_properties):
+    def __init__(self, microscope, acquisition_properties, verbose):
         threading.Thread.__init__(self)
         self.microscope = microscope
         self.acquisition_properties = acquisition_properties
+        self.verbose = verbose
 
     def run(self):
-        print("Starting tilting at " + str(time.ctime(time.time())))
+        if self.verbose:
+            print("Starting tilting at " + str(time.ctime(time.time())))
 
         self.microscope.set_stage_position_alpha(self, alpha=self.acquisition_properties.alpha_arr[-1],
                                                  speed=self.acquisition_properties.tilt_speed, movement_type="go")
 
-        print("Stopping tilting at " + str(time.ctime(time.time())))
-
-
-class TakingImagesThread (threading.Thread):
-
-    def __init__(self, microscope, acquisition_properties):
-        threading.Thread.__init__(self)
-        self.microscope = microscope
-        self.acquisition_properties = acquisition_properties
-
-    def run(self):
-        print("Starting taking images at " + str(time.ctime(time.time())))
-
-        self.microscope.set_stage_position_alpha(self, alpha=self.acquisition_properties.alpha_arr[-1],
-                                                 speed=self.acquisition_properties.tilt_speed, movement_type="go")
-
-        print("Stopping taking images at " + str(time.ctime(time.time())))
+        if self.verbose:
+            print("Stopping tilting at " + str(time.ctime(time.time())))
 
 
 if __name__ == "__main__":
@@ -118,5 +112,5 @@ if __name__ == "__main__":
     print("Shifts:")
     print(shifts_)
 
-    perform_tilt_series(microscope=scope, acquisition_properties=aqc_props, shifts=shifts_)
+    perform_tilt_series(microscope=scope, acquisition_properties=aqc_props, shifts=shifts_, verbose=True)
 
