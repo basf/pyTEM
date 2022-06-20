165980ee05fb355ad322d99e6872693ee4055289
Michael Luciuk <michael.luciuk@basf.com>
2022-06-20 12:18:11 +0200

    Overriding __str__() method

--- uED/lib/AcquisitionProperties.py
+++ uED/lib/AcquisitionProperties.py
@@ -10,7 +10,7 @@ class AcquisitionProperties:
 
     Public Attributes:
         camera_name:      str:              The name of the camera being used.
-        alpha_arr         array of floats:  An array of the tilt acquisition's alpha start-stop values.
+        alpha_arr:        array of floats:  An array of the tilt acquisition's alpha start-stop values.
         alphas:           array of floats:  The middle alpha values of the tilt acquisition.
         integration_time: int:              Total exposure time for a single image in seconds.
         sampling:         str:              Photo resolution.
@@ -49,3 +49,28 @@ class AcquisitionProperties:
         self.out_file = out_file
         self.alphas = alpha_arr[0:-1] + self.alpha_step / 2
         self.tilt_speed = 1.4768 * (self.alpha_step / integration_time) + 0.0001  # TODO: Confirm
+
+    def __str__(self):
+        return "Name of the camera being used: " + str(self.camera_name) + \
+                "\nAn array of the tilt acquisition's alpha start-stop values: " + str(self.alpha_arr) + \
+                "\nThe middle alpha values of the tilt acquisition: " + str(self.alphas) + \
+                "\nTotal exposure time for a single image: " + str(self.integration_time) + " [seconds]" + \
+                "\nPhoto resolution: " + str(self.sampling) + \
+                "\nOut file path: " + str(self.out_file) + \
+                "\nAlpha tilt speed: " + str(self.tilt_speed) + " [Thermo Fisher speed units]"
+
+
+if __name__ == "__main__":
+
+    import numpy as np
+
+    start_alpha = -35
+    stop_alpha = 30
+    step_alpha = 1
+    num_alpha = int((stop_alpha - start_alpha) / step_alpha + 1)
+    alpha_arr_ = np.linspace(start=start_alpha, stop=stop_alpha, num=num_alpha, endpoint=True)
+
+    acq_prop = AcquisitionProperties(camera_name="BM-Ceta", alpha_arr=alpha_arr_, integration_time=3, sampling='1k',
+                                     out_file=None)
+
+    print(acq_prop)
