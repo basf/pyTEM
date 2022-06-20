68edfd93b95f1c6f1e97b2f48a5941b9e51ced55
Michael Luciuk <michael.luciuk@basf.com>
2022-06-20 16:37:53 +0200

    Working to package

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
 from setuptools import setup
 
-if __name__ == '__main__':
-    setup()
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='TEMInterface',
+    version='0.1.0',
+    description='A Python wrapper for the Thermo Fisher Scientific microscope scripting interface',
+    long_description=long_description,
+    url='https://github.com/mrl280/tem-scripting-package/',
+    author='BASF TEM Laboratory',
+    author_email='philipp.mueller @ basf.com',
+    license='MIT',
+    packages=['TEMInterface', 'uED'],
+    python_requires='>=3.6',
+    install_requires=['numpy',
+                      'pandas',
+                      'hyperspy',
+                      'scipy',
+                      'tifffile',
+                      'opencv-python',
+                      'comtypes',
+                      'matplotlib',
+                      ],
+)
