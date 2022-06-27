302d0057ca4ed66f8579e63fc83574bd6af08f2d
Michael Luciuk <michael.luciuk@basf.com>
2022-06-27 09:54:37 +0200

    Type hint was creating a separate root window which was preventing scripts from continuing.
    Also adding clause for if we try to use from within the test directory.

--- pyTEM/lib/ued/add_basf_icon_to_tkinter_window.py
+++ pyTEM/lib/ued/add_basf_icon_to_tkinter_window.py
@@ -8,7 +8,7 @@ import tkinter as tk
 from tkinter import TclError
 
 
-def add_basf_icon_to_tkinter_window(root: tk.Tk()) -> None:
+def add_basf_icon_to_tkinter_window(root: tk.Tk) -> None:
     """
     Add the BASF ico to root.
     :param root: tkinter window:
@@ -16,6 +16,12 @@ def add_basf_icon_to_tkinter_window(root: tk.Tk()) -> None:
     :return: None.
     """
     try:
+        # Calling from within pyTEM
         root.iconbitmap(pathlib.Path().resolve() / "lib" / "ued" / "ico" / "BASF.ico")
     except TclError:
-        root.iconbitmap(pathlib.Path().resolve().parent.resolve() / "lib" / "ued" / "ico" / "BASF.ico")
+        try:
+            # Calling from within pyTEM.lib
+            root.iconbitmap(pathlib.Path().resolve().parent.resolve() / "ued" / "ico" / "BASF.ico")
+        except TclError:
+            # Calling from somewhere in test
+            root.iconbitmap(pathlib.Path().resolve().parent.resolve().parent.resolve() / "pyTEM" / "lib" / "ued" / "ico" / "BASF.ico")
