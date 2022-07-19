e52ba22a772b5adbfb67d94ddb3c9087770ecc68
Michael Luciuk <michael.luciuk@basf.com>
2022-07-19 16:04:59 +0200

    Get in and out file information from the user.

--- scripts/lib/GetInOutFile.py
+++ scripts/lib/GetInOutFile.py
@@ -1,43 +1,23 @@
 """
  Author:  Michael Luciuk
  Date:    Summer 2022
-
-
-Read in some images (possibly from a stack, possibly from a bunch of single image files), align the images, and save
- the results back to file.
-
-Image shifts are estimated using Hyperspy's estimate_shift2D() function. This function uses a phase correlation
-algorithm based on the following paper:
-    Schaffer, Bernhard, Werner Grogger, and Gerald Kothleitner. “Automated Spatial Drift Correction for EFTEM
-     Image Series.” Ultramicroscopy 102, no. 1 (December 2004): 27–36.
 """
 
 import os
 import sys
-import pathlib
-import mrcfile
 
 import tkinter as tk
-import numpy as np
 
 from tkinter import ttk, filedialog
 from typing import Tuple, Union
 from pathlib import Path
 
-# Add the pyTEM package directory to path
-package_directory = pathlib.Path().resolve().parent.resolve().parent.resolve().parent.resolve()
-sys.path.append(str(package_directory))
-try:
-    from pyTEM.lib.interface.Acquisition import Acquisition
-    from pyTEM.lib.interface.AcquisitionSeries import AcquisitionSeries
-    from pyTEM.lib.micro_ed.add_basf_icon_to_tkinter_window import add_basf_icon_to_tkinter_window
-except Exception as ImportException:
-    raise ImportException
+from pyTEM.lib.micro_ed.add_basf_icon_to_tkinter_window import add_basf_icon_to_tkinter_window
 
 
 class GetInOutFile:
     """
-    Get the in and out file info from the user.
+    Display a Tkinter window, and get the in and out file info from the user.
     """
 
     def __init__(self):
@@ -211,58 +191,3 @@ class GetInOutFile:
         # Build and return the complete path
         out_path = self.out_directory + "/" + str(out_file_name.get()) + str(file_extension.get())
         return self.in_file, out_path
-
-
-# Preallocate.
-acq_series = AcquisitionSeries()
-
-# Get the in and out file info from the user.
-in_file, out_file = GetInOutFile().run()
-
-if isinstance(in_file, str):
-    # Then we have a single file, assume it is an image stack.
-
-    if in_file[-4:] == ".mrc":
-        # Then it is an MRC file (probably).
-        with mrcfile.open(in_file, permissive=True) as mrc:
-            image_stack_arr = mrc.data
-            extended_header = mrc.extended_header
-
-        # Load the images into an acquisition series object.
-        images = np.shape(image_stack_arr)[0]
-        for i in range(images):
-            acq_series.append(Acquisition(image_stack_arr[i]))
-
-        # Go ahead and actually perform the alignment
-        acq_series = acq_series.align()
-
-        # Save the results to file.
-        with mrcfile.new(out_file, overwrite=True) as mrc:
-            mrc.set_data(acq_series.get_image_stack())
-            mrc.set_extended_header(extended_header)
-
-        exit()  # We are done here
-
-    elif out_file[-4:] == ".tif" or out_file[-5:] == ".tiff":
-        raise NotImplementedError("We don't know how to deal with TIFF stacks yet!")
-
-    else:
-        raise Exception("File type unknown.")
-
-else:
-    # We have a bunch of single images in separate files.
-    for file in in_file:
-        acq_series.append(Acquisition(file))
-
-# Go ahead and perform the alignment using AcquisitionSeries' align() method.
-acq_series = acq_series.align()
-
-# Save to file.
-if out_file[-4:] == ".mrc":
-    acq_series.save_as_mrc(out_file=out_file)
-
-elif out_file[-4:] == ".tif" or out_file[-5:] == ".tiff":
-    acq_series.save_as_tif(out_file=out_file)
-
-else:
-    raise Exception("Error: Out file type not recognized: " + str(out_file))
