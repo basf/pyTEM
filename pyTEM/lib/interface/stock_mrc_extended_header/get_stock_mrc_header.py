ec4cfabf0d8a2360503b54275381d82490a876f9
Michael Luciuk <michael.luciuk@basf.com>
2022-07-21 15:59:16 +0200

    Obtaining the path to the extended header file in a more robust way.

--- pyTEM/lib/interface/stock_mrc_extended_header/get_stock_mrc_header.py
+++ pyTEM/lib/interface/stock_mrc_extended_header/get_stock_mrc_header.py
@@ -1,6 +1,11 @@
-import pathlib
+"""
+ Author:  Michael Luciuk
+ Date:    Summer 2022
+"""
 
+import os
 import numpy as np
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_stock_mrc_extended_header() -> np.ndarray:
@@ -8,9 +13,8 @@ def get_stock_mrc_extended_header() -> np.ndarray:
     Read in and return a "stock" extended header. This extended header will allow MRC files to be returned to
     :return:
     """
-    header_file = pathlib.Path(__file__).parent.resolve() / "stock_mrc_extended_header.npy"
-    print(header_file)
-    extended_header = np.load(str(header_file))
+    extended_header_path = os.path.join(BASEDIR, "stock_mrc_extended_header.npy")
+    extended_header = np.load(str(extended_header_path))
     return extended_header
 
 
