8e587520d5fc2437be9961f50bb60e91fe7c8613
Michael Luciuk <michael.luciuk@basf.com>
2022-07-21 15:55:31 +0200

    Removing unnecessary print statements

--- pyTEM/lib/micro_ed/add_basf_icon_to_tkinter_window.py
+++ pyTEM/lib/micro_ed/add_basf_icon_to_tkinter_window.py
@@ -16,6 +16,4 @@ def add_basf_icon_to_tkinter_window(root: tk.Tk) -> None:
     :return: None.
     """
     path_to_ico = os.path.join(BASEDIR, "ico\\BASF.ico")
-    print("Path to ico: " + str(path_to_ico))
-    print(type(path_to_ico))
     root.iconbitmap(path_to_ico)
