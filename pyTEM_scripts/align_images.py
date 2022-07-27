f51aebfb7e39db861ddf15dda6b02d796d285f81
Michael Luciuk <michael.luciuk@basf.com>
2022-07-27 10:30:21 +0200

    Now supports load from any image stack files that can be loaded by AcquisitionSeries, including tiff stacks.

--- pyTEM_scripts/align_images.py
+++ pyTEM_scripts/align_images.py
@@ -3,9 +3,7 @@
  Date:    Summer 2022
 """
 
-import mrcfile
-
-import numpy as np
+from typing import Tuple
 
 from pyTEM.lib.interface.Acquisition import Acquisition
 from pyTEM.lib.interface.AcquisitionSeries import AcquisitionSeries
@@ -17,51 +15,28 @@ def align_images():
     Read in some images (possibly from a stack, possibly from a bunch of single image files), align the images, and save
      the results back to file.
 
-    Image alignment is performed using
+    Image shifts are estimated using Hyperspy's estimate_shift2D() function. This function uses a phase correlation
+     algorithm based on the following paper:
+        Schaffer, Bernhard, Werner Grogger, and Gerald Kothleitner. “Automated Spatial Drift Correction for EFTEM
+        Image Series.” Ultramicroscopy 102, no. 1 (December 2004): 27–36.
 
     :return: None.
     """
-    # Preallocate.
-    acq_series = AcquisitionSeries()
-
     # Get the in and out file info from the user.
     in_file, out_file = GetInOutFile().run()
 
     if isinstance(in_file, str):
         # Then we have a single file, assume it is an image stack.
+        acq_series = AcquisitionSeries(source=in_file)
 
-        if in_file[-4:] == ".mrc":
-            # Then it is an MRC file (probably).
-            with mrcfile.open(in_file, permissive=True) as mrc:
-                image_stack_arr = mrc.data
-                extended_header = mrc.extended_header
-
-            # Load the images into an acquisition series object.
-            images = np.shape(image_stack_arr)[0]
-            for i in range(images):
-                acq_series.append(Acquisition(image_stack_arr[i]))
-
-            # Go ahead and actually perform the alignment and save in this conditional to preserve the metadata stored
-            #  in the extended header.
-            acq_series = acq_series.align()
-
-            # Save the results to file.
-            with mrcfile.new(out_file, overwrite=True) as mrc:
-                mrc.set_data(acq_series.get_image_stack())
-                mrc.set_extended_header(extended_header)
-
-            return  # We are done here
-
-        elif out_file[-4:] == ".tif" or out_file[-5:] == ".tiff":
-            raise NotImplementedError("We don't know how to deal with TIFF stacks yet!")  # TODO
-
-        else:
-            raise Exception("File type unknown.")
-
-    else:
+    elif isinstance(in_file, Tuple):
         # We have a bunch of single images in separate files.
+        acq_series = AcquisitionSeries()
         for file in in_file:
             acq_series.append(Acquisition(file))
+
+    else:
+        raise Exception("Error: in_file type not recognized: " + str(type(in_file)))
 
     # Go ahead and perform the alignment using AcquisitionSeries' align() method.
     acq_series = acq_series.align()
