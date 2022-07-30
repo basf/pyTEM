ca0fcd7e7094989cf4854f1914ac42d6fc533ccb
Michael Luciuk <michael.luciuk@basf.com>
2022-07-30 17:22:20 +0200

    Making the num_channels check more explicit.

--- pyTEM/lib/rgb_to_greyscale.py
+++ pyTEM/lib/rgb_to_greyscale.py
@@ -12,17 +12,22 @@ def rgb_to_greyscale(rgb_image: np.ndarray) -> np.ndarray:
      unsigned 8-bit greyscale.
 
     :param rgb_image: 3-dimensional array:
-        The RGB image, as a 3-dimensional array.
+        A 2-dimensional RGB image, as a 3-dimensional array.
+
     :return: 2-dimensional array:
-        The provided array, but as a greyscale image.
+        The provided 2-dimensional image, but in greyscale.
     """
+    # Verify the dimensionality of the provided array.
     if rgb_image.ndim != 3:
         raise Exception("Error: rgb_to_greyscale() input should be 3-dimensional but it is "
                         + str(rgb_image.ndim) + ".")
-    if np.shape(rgb_image)[2] not in {3, 4}:
-        # If 4 channels it is RGBA or RGBX in which case the 4th channel is ignored in the conversion.
+
+    # Verify we have the expected number of channels. If 4 channels it is RGBA or RGBX in which case the 4th channel
+    #  is ignored in the conversion.
+    _, _, num_channels = np.shape(rgb_image)
+    if num_channels not in {3, 4}:
         raise Exception("Error: rgb_to_greyscale() input should contain 3 or 4 channels in the last dimension, but "
-                        "there are " + str(np.shape(rgb_image)[2]) + ".")
+                        "there are " + str(num_channels) + ".")
 
     r, g, b = rgb_image[:, :, 0], rgb_image[:, :, 1], rgb_image[:, :, 2]
     return 0.2989 * r + 0.5870 * g + 0.1140 * b
