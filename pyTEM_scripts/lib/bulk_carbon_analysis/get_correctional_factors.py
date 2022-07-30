f04a0d4faefd13e73dabf446cc914b9e9ca79d5a
Michael Luciuk <michael.luciuk@basf.com>
2022-07-30 12:08:45 +0200

    Layout complete, still need at add logic

--- pyTEM_scripts/lib/bulk_carbon_analysis/get_correctional_factors.py
+++ pyTEM_scripts/lib/bulk_carbon_analysis/get_correctional_factors.py
@@ -82,12 +82,12 @@ def get_correctional_factors() -> Union[np.ndarray, None]:
     instructions2_label.config(foreground="blue")
 
     # Create analyzer and polarizer table labels.
-    analyzer_angle_label = ttk.Label(root, text="Analyzer Angle, \u03C6 [deg]", wraplength=window_width, font=font,
+    analyzer_angle_label = ttk.Label(root, text="Analyzer Angle, \u03C6 [deg]", wraplength=125, font=font,
                                      justify='center')
     analyzer_angle_label.grid(row=4, column=2, padx=5, pady=5)
-    polarizer_angle_label = ttk.Label(root, text="Polarizer Angle, \u03B8 [deg]", wraplength=window_width, font=font,
+    polarizer_angle_label = ttk.Label(root, text="Polarizer Angle, \u03B8 [deg]", wraplength=125, font=font,
                                       justify='center')
-    polarizer_angle_label.grid(row=6, column=0, padx=5, pady=5)
+    polarizer_angle_label.grid(row=6, rowspan=2, column=0, padx=5, pady=5)
 
     # Add horizontal and vertical degree labels.
     horizontal_deg_labels = np.asarray([ttk.Label(root, text=str(45 * i), font=font) for i in range(4)])
@@ -95,37 +95,44 @@ def get_correctional_factors() -> Union[np.ndarray, None]:
         label.grid(row=5, column=2 + i, padx=5, pady=5)
     vertical_deg_labels = np.asarray([ttk.Label(root, text=str(45 * i), font=font) for i in range(4)])
     for i, label in enumerate(vertical_deg_labels):
-        label.grid(row=6 + i, column=1, padx=5, pady=5)
+        label.grid(row=6 + 2 * i, rowspan=2, column=1, padx=5, pady=5)
 
     # Preallocate a matrix for the correctional factors, initially, they are all just 1.0.
-    correctional_factors = np.full(shape=(4, 4), fill_value=1.0, dtype='float64')
+    correctional_factor_str_vars = observed_intensity_entry_boxes = np.asarray(
+        [[tk.StringVar() for j in range(4)] for i in range(4)]
+    )
 
     # Offsets for the 4x4 entry box grid.
     row_offset = 6  # There are this many rows above us.
     col_offset = 2  # There are this many columns to the right of us.
 
+    # Add entry boxes in which the user can insert the observed intensity.
     observed_intensity_entry_boxes = np.asarray(
-        [[ttk.Entry(root, textvariable=correctional_factors[i][j]) for j in range(4)] for i in range(4)]
+        [[ttk.Entry(root, textvariable=correctional_factor_str_vars[i][j]) for j in range(4)] for i in range(4)]
     )
+    for (row, col), observed_intensity_entry_box in np.ndenumerate(observed_intensity_entry_boxes):
+        observed_intensity_entry_box.config(justify='center')
+        observed_intensity_entry_box.insert(0, str(100))
+        observed_intensity_entry_box.grid(row=row_offset + 2 * row, column=col_offset + col, padx=5, pady=(5, 0))
 
-    # Add labels where we show the correctional factors in blue.
+    # Add labels where we show the current correctional factors in blue.
     correctional_factor_labels = np.asarray(
         [[ttk.Label(root, text=str(i) + str(j), font=font) for j in range(4)] for i in range(4)]
     )
     for (row, col), correctional_factor_label in np.ndenumerate(correctional_factor_labels):
         correctional_factor_label.config(foreground="blue")
-        correctional_factor_label.grid(row=row_offset + row, column=col_offset + col, padx=5, pady=5)
+        correctional_factor_label.grid(row=row_offset + 2 * row + 1, column=col_offset + col, padx=5, pady=(0, 5))
 
 
 
 
     # Create a 'Continue' button that the user can click to proceed.
     continue_button = ttk.Button(root, text="Continue", command=lambda: root.destroy(), style="big.TButton")
-    continue_button.grid(row=10, column=0, columnspan=6, padx=5, pady=5)
+    continue_button.grid(row=14, column=0, columnspan=6, padx=5, pady=5)
 
     # Create an 'exit' button that the user can use to exit the script.
     exit_button = ttk.Button(root, text="Exit", command=lambda: sys.exit(), style="big.TButton")
-    exit_button.grid(row=11, column=0, columnspan=6, padx=5, pady=5)
+    exit_button.grid(row=15, column=0, columnspan=6, padx=5, pady=5)
 
     style.configure('big.TButton', font=(None, 10), foreground="blue4")
     style.configure('big.TCheckbutton', font=(None, 12, 'bold'))
@@ -137,6 +144,9 @@ def get_correctional_factors() -> Union[np.ndarray, None]:
 
     if use_correctional_factors.get():
         # The user has requested we proceed with correctional factors.
+        correctional_factors = np.asarray(
+            [[correctional_factor_str_vars[i][j].get() for j in range(4)] for i in range(4)]
+        )
         return correctional_factors
 
     else:
