8f5b609a4f322f0fad1ed533a280291e7667cf46
Michael Luciuk <michael.luciuk@basf.com>
2022-08-01 14:12:18 +0200

    Restore the microscope to imaging mode upon script exit.

--- pyTEM_scripts/lib/micro_ed/exit_script.py
+++ pyTEM_scripts/lib/micro_ed/exit_script.py
@@ -11,10 +11,12 @@ from pyTEM.Interface import Interface
 
 def exit_script(microscope: Union[Interface, None], status: int) -> None:
     """
-    Return the microscope to a safe state, zero the alpha tilt, zero the image and beam shifts, and exit the
-     MicroED script.
-    :param microscope: pyTEM (or None):
+    Return the microscope to a safe state, zero the alpha tilt, zero the image and beam shifts, and restore the
+     projection mode to 'imaging'.
+
+    :param microscope: pyTEM.Interface (or None):
         The microscope interface.
+
     :param status: int:
         Exit status, one of:
             1: Early exit (script not yet complete)
@@ -23,8 +25,9 @@ def exit_script(microscope: Union[Interface, None], status: int) -> None:
     """
     if microscope is not None:
         warnings.warn("Returning the microscope to a safe state, zeroing the alpha tilt, "
-                      "and zeroing the image and beam shifts...")
+                      "zeroing the image and beam shifts, and restoring the projection mode to 'imaging'...")
         microscope.make_safe()
         microscope.set_stage_position_alpha(alpha=0)
         microscope.zero_shifts()
+        microscope.set_projection_mode(new_projection_mode="imaging")
     sys.exit(status)
