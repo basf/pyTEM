ffce4595d1d34d1962e6d4566551cf881fddd589
Michael Luciuk <michael.luciuk@basf.com>
2022-08-01 10:07:19 +0200

    Adding verbose parameter and goodbye message.

--- pyTEM_scripts/align_images.py
+++ pyTEM_scripts/align_images.py
@@ -10,6 +10,7 @@ from typing import Tuple
 from pyTEM.lib.Acquisition import Acquisition
 from pyTEM.lib.AcquisitionSeries import AcquisitionSeries
 from pyTEM_scripts.lib.align_images.GetInOutFile import GetInOutFile
+from pyTEM_scripts.lib.align_images.display_goodbye_message import display_goodbye_message
 
 DESCRIPTION = "Read in some images (possibly from an image stack, possibly from a bunch of single image files), " \
               "align the images, and save the results back to file." \
@@ -20,43 +21,68 @@ DESCRIPTION = "Read in some images (possibly from an image stack, possibly from 
               "\n       for EFTEMmImage Series.” Ultramicroscopy 102, no. 1 (December 2004): 27–36."
 
 
-def align_images():
+def align_images(verbose: bool = False):
     """
     Read in some images (possibly from a stack, possibly from a bunch of single image files), align the images, and
      save the results back to file. For more information, please refer to the above description, or view using:
 
         align_images --help
 
+    :param verbose: bool:
+        Print out extra information. Useful for debugging.
+
     :return: None. The resulting image stack is saved to file at the location requested by the user.
     """
     # Get the in and out file info from the user.
+    if verbose:
+        print("Prompting the user for in/out file path information...")
     in_file, out_file = GetInOutFile().run()
 
     if isinstance(in_file, str):
         # Then we have a single file, assume it is an image stack.
+        if verbose:
+            print("Reading in image stack...")
         acq_series = AcquisitionSeries(source=in_file)
 
     elif isinstance(in_file, Tuple):
         # We have a bunch of single images in separate files.
+        num_in_files = len(in_file)
+        if verbose:
+            print("Reading in single image files...")
+
         acq_series = AcquisitionSeries()
-        for file in in_file:
+        for i, file in enumerate(in_file):
+            if verbose:
+                print(" Loading image " + str(i + 1) + " out of " + str(num_in_files) + "...")
             acq_series.append(Acquisition(file))
 
     else:
         raise Exception("Error: in_file type not recognized: " + str(type(in_file)))
 
     # Go ahead and perform the alignment using AcquisitionSeries' align() method.
+    if verbose:
+        print("Performing image alignment...")
     acq_series = acq_series.align()
+    if verbose:
+        print("Image alignment complete...")
 
     # Save to file.
     if out_file[-4:] == ".mrc":
+        if verbose:
+            print("Saving to file as MRC...")
         acq_series.save_as_mrc(out_file=out_file)
 
     elif out_file[-4:] == ".tif" or out_file[-5:] == ".tiff":
+        if verbose:
+            print("Saving to file as TIFF...")
         acq_series.save_as_tif(out_file=out_file)
 
     else:
         raise Exception("Error: Out file type not recognized: " + str(out_file))
+
+    if verbose:
+        print("Displaying goodbye message...")
+    display_goodbye_message(out_path=out_file)
 
 
 def script_entry():
@@ -68,9 +94,11 @@ def script_entry():
 
     """
     parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=argparse.RawTextHelpFormatter)
-    parser.parse_args()
-    align_images()
+    parser.add_argument("-v", "--verbose", help="Increase verbosity; especially useful when debugging.",
+                        action="store_true")
+    args = parser.parse_args()
+    align_images(verbose=args.verbose)
 
 
 if __name__ == "__main__":
-    align_images()
+    align_images(verbose=True)
---
+++ pyTEM_scripts/lib/align_images/display_goodbye_message.py
@@ -0,0 +1,73 @@
+"""
+ Author:  Michael Luciuk
+ Date:    Summer 2022
+"""
+
+import pathlib
+import sys
+import tkinter as tk
+
+from tkinter import ttk
+from typing import Union
+
+from pyTEM_scripts.lib.micro_ed.add_basf_icon_to_tkinter_window import add_basf_icon_to_tkinter_window
+
+
+def display_goodbye_message(out_path: Union[pathlib.Path, str]):
+    """
+    Display a goodbye message,
+        - thanking the user,
+        - letting them know where to find the alignment results,
+        - and prompting them to report any issues on the GitHub.
+
+    :param out_path: str or path:
+        Path to the alignment results.
+
+    :return: None.
+    """
+    root = tk.Tk()
+    style = ttk.Style()
+    window_width = 650
+
+    root.title("Mission success!")
+    add_basf_icon_to_tkinter_window(root)
+
+    "We have now completed the MicroED tilt series acquisition!" \
+
+    upper_message = "Image Alignment Successful!" \
+                    "\n\nThe results can be found here:"
+    lower_message = "Thank you for using pyTEM, please report any issues on GitHub:"
+
+    # Display the upper part of the message.
+    upper_message_label = ttk.Label(root, text=upper_message, wraplength=window_width, font=(None, 15),
+                                    justify='center')
+    upper_message_label.grid(column=0, row=0, padx=5, pady=(5, 0))
+
+    # Display the out file path.
+    out_path_label = ttk.Label(root, text=out_path, wraplength=window_width, font=(None, 15, 'bold'),
+                               justify='center')
+    out_path_label.grid(column=0, row=1, padx=5, pady=(0, 5))
+
+    # Display the lower part of the message.
+    upper_message_label = ttk.Label(root, text=lower_message, wraplength=window_width, font=(None, 15),
+                                    justify='center')
+    upper_message_label.grid(column=0, row=2, padx=5, pady=(5, 0))
+
+    # Display the link to the GitHub issues board.
+    github_issues_label = ttk.Label(root, text="https://github.com/mrl280/pyTEM/issues", wraplength=window_width,
+                                    font=(None, 15, 'bold'), justify='center')
+    github_issues_label.grid(column=0, row=3, padx=5, pady=(0, 5))
+
+    # Create exit button.
+    exit_button = ttk.Button(root, text="Exit", command=lambda: sys.exit(0), style="big.TButton")
+    exit_button.grid(column=0, row=4, padx=5, pady=5)
+    style.configure('big.TButton', font=(None, 10), foreground="blue4")
+
+    # Center the window on the screen
+    root.eval('tk::PlaceWindow . center')
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    display_goodbye_message(out_path="Some fake path...")
